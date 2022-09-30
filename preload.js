2f13d453397c1eece6e10bbbb0be2270d9f39477
Nigel Kibodeaux <nigelmail@gmail.com>
2022-09-30 10:05:23 -0800

    updated electron

--- preload.js
+++ preload.js
@@ -1,1 +1,23 @@
-window.ipcRenderer = require('electron').ipcRenderer
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    showLogs: () => ipcRenderer.send('show-logs'),
+
+    authUpdated: ({ domain, password }) => ipcRenderer.send('authUpdated', { domain, password }),
+
+    startDownload: ({ network, channel, user, pack }) => ipcRenderer.send('start-download', { network, channel, user, pack }),
+
+    selectDownloadDirectory: () => ipcRenderer.send('select-download-directory'),
+
+    getDownloadDirectory: () => ipcRenderer.send('getSetting', 'downloadDirectory'),
+
+    killDownload: (id) => ipcRenderer.ipc.send('kill-download', id),
+
+    onStateUpdate: (callback) => ipcRenderer.on('state-update', callback),
+
+    onLinkClicked: (callback) => ipcRenderer.on('link-clicked', callback),
+
+    onLog: (callback) => ipcRenderer.on('log', callback),
+
+    onDownloadDirectorySet: (callback) => ipcRenderer.on('download-directory-set', callback),
+})
